ab93f313b68d3f5108c142b9ac517b1dc23117d8
S1ro <matej.sirovatka@gmail.com>
2023-06-16 23:04:22 +0200

    Temporary fix for config file

--- config.py
+++ config.py
@@ -1,11 +1,27 @@
 import yaml
-
+import os
 
 class Config(dict):
 
     def __init__(self):
-        with open("config.yaml", "r") as f:
-            data = yaml.safe_load(f)
+        keys = ["bot_token", "server_id", "semester_message", "bot_channel"]
+        try:
+            with open("config.yaml", "r") as f:
+                data = yaml.safe_load(f)
+        except:
+            for key in keys:
+                if key not in os.environ:
+                    raise Exception(f"Missing environment variable {key}")
+                val = os.environ[key]
+
+                try:
+                    val = int(val)
+                except:
+                    pass
+
+                data[key] = val
+                
+
         super().__init__(data)
 
 
